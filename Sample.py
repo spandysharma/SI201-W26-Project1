3a33bd4d51a11052b3ee121184045b06187b5312
spandysharma <112590656+spandysharma@users.noreply.github.com>
2026-02-03 07:45:14 -0500

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,9 @@
+# Name:
+# Student ID:
+# Email:
+# Who or what you worked with on this homework (including generative AI like ChatGPT):
+# If you worked with generative AI also add a statement for how you used it.
+# e.g.:
+# Asked ChatGPT hints for debugging and suggesting the general structure of the code
+# Did your use of GenAI on this assignment align with your goals and guidelines in 
+#    your Gen AI contract? If not, why?
---
+++ Video_Links.py
@@ -0,0 +1,6 @@
+# Please add Google Drive folder's link below.
+# The folder must include each member's video.
+# Please name each video as Member1_Video, Member2_Video etc.
+
+# TO-DO
+# Link:
